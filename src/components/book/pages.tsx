10995374c6d1a55d9e540b10ca0cfb08b991070f
rajdalsaniya1995ml <rajdalsaniya1995.ml@gmail.com>
2026-09-13 21:10:56 +0530

    Update pages.tsx

--- src/components/book/pages.tsx
+++ src/components/book/pages.tsx
@@ -47,11 +47,11 @@ const stop = (e: React.MouseEvent) => e.stopPropagation();
 
 export function PageShell({ children }: { children: ReactNode }) {
   return (
-   <div className="page-imperfections deckle-edge relative h-full w-full px-7 py-8 sm:px-9">
+    <div className="page-imperfections deckle-edge relative h-full w-full px-7 py-8 sm:px-9">
       <div className="relative z-10 h-full min-h-0">
         {children}
       </div>
-    </div>7
+    </div>
   );
 }
 
